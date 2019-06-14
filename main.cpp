d80646bfe2829121881c082fff3f046f7fd9aea0
Jason <mickeu420@gmail.com>
2019-06-14 18:57:09 -0400

    simple_anim

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@
 using namespace std;
 
 int main() {
+  //cout << "hi";
     ifstream file("simple_anim.mdl");
     Parser p;
+    //cout << "before lex";
     p.lex(file);
+    //cout << "after lex";
     p.parse();
+    //    cout << "after parse";
     return 0;
 }
